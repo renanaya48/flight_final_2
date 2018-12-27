4922a876e3874a5624906488849bb0648cba0609
renanaya48 <45995947+renanaya48@users.noreply.github.com>
2018-12-27 15:28:29 +0200

    Update VarCommand.h

--- VarCommand.h
+++ VarCommand.h
@@ -10,12 +10,27 @@
 
 using namespace std;
 
+/**
+ * 
+ * @param vectorIt the vector
+ * @return 0 when done
+ */
 class VarCommand: public Command{
+    //member
     SymbolTable* symbols;
 public:
+     /**
+     * constructor
+     * @param s  SymbolTable
+     */
     VarCommand(SymbolTable* s){
         this->symbols = s;
     }
+     /**
+     * execute
+     * @param vectorIt the vector
+     * @return 0 when done
+     */
     virtual int execute(vector<string>::iterator &vectorIt);
 };
 
